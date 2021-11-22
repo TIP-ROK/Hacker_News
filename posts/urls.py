b40baece5acc745fe30f143e5dd40762bc2a5f3b
TIP-ROK <chenye797@gmail.com>
2021-11-22 18:03:38 +0600

    updating first adding

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostListCreateView.as_view()),
     path('<int:pk>/', views.PostDetailView.as_view()),
+    path('<int:pk>/comment/', views.CommentCreateView.as_view()),
+    path('<int:pk>/comment_detail/', views.CommentDetailView.as_view()),
+    path('<int:pk>/upvote/', views.upvote),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,20 @@
-from rest_framework import generics
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics, permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-from .serializer import PostSerializer
-from .models import Post
+from .models import Post, Comment
+from .permissions import IsOwnerOrReadOnly
+from .serializer import PostSerializer, CommentSerializer, PostDetailSerializer
 
 
 class PostListCreateView(generics.ListCreateAPIView):
+    """
+        This API endpoint allows to get list of post and create post
+    """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -14,4 +22,36 @@ class PostListCreateView(generics.ListCreateAPIView):
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class CommentCreateView(generics.CreateAPIView):
+    """
+        This API endpoint allows to get list of comment and create comment
+    """
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        post = Post.objects.get(pk=self.kwargs['pk'])
+        serializer.save(author=self.request.user, post=post)
+
+
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+@csrf_exempt
+@api_view(['POST'])
+def upvote(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.user in post.upvotes.all():
+        post.upvotes.remove(request.user)
+        return Response(data={'message': f'{request.user.username}'}, status=status.HTTP_201_CREATED)
+    else:
+        post.upvotes.add(request.user)
+        return Response(data={'message': ''}, status=status.HTTP_202_ACCEPTED)
